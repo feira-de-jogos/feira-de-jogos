d690d369cbbc681cac9d864ff6c8bce88e1ae02e
Ederson Torresini <boidacarapreta@gmail.com>
2023-10-27 14:53:29 -0300

    Adicionar suporte a HTTPS

--- dispensador-de-comida/dispensador-de-comida.ino
+++ dispensador-de-comida/dispensador-de-comida.ino
@@ -7,7 +7,7 @@
 // DEFINIÇÕES DE REDE/MQTT
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xEF};
 EthernetClient ethClient;
-String server = "https://feira-de-jogos.sj.ifsc.edu.br/api/v1/debito";
+String URL = "https://feira-de-jogos.sj.ifsc.edu.br/api/v1/debito";
 HTTPClient http;
 
 // DEFINIÇÕES DO DISPLAY: >> EN, RS, DB4, DB5, DB6, DB7 >> PSB (HIGH) >> R/W (LOW)
@@ -31,13 +31,15 @@ Keypad teclado = Keypad(makeKeymap(TECLAS_MATRIZ), PINOS_LINHAS, PINOS_COLUNAS, 
 String senha = ""; // Senha utilizada para liberacao
 
 // MÉTODO PARA IMPRIMIR LINHA NO DISPLAY
-void LCD_Linha(int a, int b, String texto) {
+void LCD_Linha(int a, int b, String texto)
+{
   lcd.setCursor(a, b); // Posiciona o cursor na Coluna a x Linha b >> L1(0,0), L2(0,2), L3(8,0), L4(8,2)
   lcd.print(texto); // Imprime o texto na posição (a,b) do LCD >> Tamanho máximo: 16 caracteres/linha
 }
 
 // MONTAGEM DAS TELAS
-void LCD_Espera() {
+void LCD_Espera()
+{
   lcd.clear();
   lcd.setCursor(0, 0); // Posiciona o cursor na Coluna a x Linha b >> L1(0,0), L2(0,2), L3(8,0), L4(8,2)
   char x = 0x08;
@@ -208,6 +210,38 @@ void setup()
 }
 
 String codigo = "Codigo: ";
+const char* rootCA= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
 
 void loop()
 {
@@ -225,16 +259,29 @@ void loop()
         LCD_Validacao();
 
         // Requisição HTTP com POST
+        http.begin(URL, rootCA)
         int httpResponseCode = http.POST();
-        if (httpResponseCode > 0)
+        switch(httpResponseCode)
         {
-          Serial.print(" HTTP Responde code: ");
-          Serial.println(httpResponseCode);
-          String payload = http.getString();
-          Serial.println(payload);
-        } else {
-          Serial.print("Error code: ");
-          Serial.println(httpResponseCode);
+          case 200:
+            Serial.println("200 OK"); // único cenário em que a operação é válida
+            break;
+          case 400:
+            Serial.println("400 Bad Request");
+            break;
+          case 401:
+            Serial.println("401 unauthorized");
+            break;
+          case 403:
+            Serial.println("403 Forbidden");
+            break;
+          case 500:
+            Serial.println("500 Internal Server Error");
+            break;
+          default:
+            Serial.print("Erro desconhecido: ")
+            Serial.println(httpResponseCode)
+            break;
         }
         http.end()
 
