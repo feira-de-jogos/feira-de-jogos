f76ee7d76f028516773b6602dfe446a63adf923a
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-03-07 12:36:05 +0000

    ReadMe atualizado

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import config from './config.js'
 class Game extends Phaser.Game {
   constructor() {
     super(config)
-    }
   }
+}
 
-  window.onload =() => {
-    window.game = new Game();
-  }
+window.onload = () => {
+  window.game = new Game();
+}
