a95b58859914aeebd40cefe5084dce7aa5faac16
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-03-22 10:51:16 +0000

    aaaa

--- cliente/js/abertura.js
+++ cliente/js/abertura.js
@@ -4,9 +4,11 @@ export default class abertura extends Phaser.Game {
   }
 
     preload(){
+      this.preload.image('fundo', 'assets/logo.png')
     }
 
     create(){
+      this.add.image(400, 225, 'fundo')
     }
 
     update(){
