537beebdd4a199d6843b2c5a9064933416667e9f
Davi <160233282+daviiq@users.noreply.github.com>
2024-03-22 09:21:51 -0300

    Configuração de áudio

--- cliente/js/abertura.js
+++ cliente/js/abertura.js
@@ -4,9 +4,16 @@ export default class abertura extends Phaser.Scene {
   }
 
   preload() {
-  }
+    this.load.image('fundo', './assets/teste.png')
+  } //colocar após a barrinha o plano de fundo, lembrando que deve importa-lo para o assets
 
   create() {
+    this.add.image(427, 240, 'fundo')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.game.scene.stop('abertura')
+        this.game.scene.start('sala')
+      })
   }
   update() {
 
