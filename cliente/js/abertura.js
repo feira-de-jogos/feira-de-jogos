5e3006a21d71c3999ed856e5fc703b9dd0391638
ViquettiIFSC <160497364+ViquettiIFSC@users.noreply.github.com>
2024-03-22 11:08:37 +0000

    22/03

--- cliente/js/abertura.js
+++ cliente/js/abertura.js
@@ -3,11 +3,17 @@ export default class abertura extends Phaser.Game {
     super('abertura')
   }
 
-    preload(){
+    preload() {
+      this.load.image('fundo', './assets/fundo.png')
     }
-
-    create(){
-    }
+    create() {
+      this.add.image(400,255,'fundo')
+        .setInteractive()
+        .on('pointerdown', () => {
+          this.game.scene.stop('abertura')
+          this.game.scene.start('sala')
+        })
+      }
 
     update(){
     }
