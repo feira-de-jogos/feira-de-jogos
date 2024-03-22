ec6cd94632b213c6024e356703874841294c55c7
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-03-22 11:59:53 +0000

    Audio e sala

--- cliente/js/sala.js
+++ cliente/js/sala.js
@@ -5,10 +5,15 @@ export default class sala extends Phaser.Scene {
   
       preload() {
         this.load.image('fundo', './assets/fundo.png')
+        this.load.audio('iniciar', './assets/iniciar.mp3')
       }
       create() {
+        this.iniciar = this.sound.add('iniciar')
         this.add.image(400,255,'fundo')
-        this.add.text(100, 50, 'sala 1').setInteractive().on('pointerdown', () => {})
+        this.add.text(100, 50, 'sala 1').setInteractive()
+        .on('pointerdown', () => {
+            this.iniciar.play()
+        })
         }
   
       update(){
