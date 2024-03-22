4c5bc4f3ce9676b8952dff5bd4116eaaa81267b5
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-03-22 12:03:29 +0000

    audio de fundo

--- cliente/js/sala.js
+++ cliente/js/sala.js
@@ -6,9 +6,11 @@ export default class sala extends Phaser.Scene {
       preload() {
         this.load.image('fundo', './assets/fundo.png')
         this.load.audio('iniciar', './assets/iniciar.mp3')
+        this.load.audio('mapa', './assets/mapa.mp3')
       }
       create() {
         this.iniciar = this.sound.add('iniciar')
+        this.sound.add('mapa', {loop:true}).play()
         this.add.image(400,255,'fundo')
         this.add.text(100, 50, 'sala 1').setInteractive()
         .on('pointerdown', () => {
