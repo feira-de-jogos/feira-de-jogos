dcb8368af5a53f5f5bf83b0a112d6d49b4a66dcc
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-03-28 12:39:26 +0000

    Animacoes

--- cliente/js/mapa.js
+++ cliente/js/mapa.js
@@ -4,19 +4,52 @@ export default class mapa extends Phaser.Scene {
     }
   
       preload() {
-        this.load.tilemapTilledJSON('mapa', '/assets/mapa/mapa.json')
+        //this.load.tilemapTilledJSON('mapa', '/assets/mapa/mapa.json')
 
-        this.load.image('bloco', '/assets/mapa/bloco.png')
+        //this.load.image('bloco', '/assets/mapa/bloco.png')
+
+        this.load.spritesheet('cavaleiro-1', './assets/entities/jumpKing.png', {frameWidth: 32, frameHeight: 32})
       }
   
       create() {
-        this.tilemapMapa = this.make.tilemap() [{
-            key: 'mapa'
-        }]
-        this.tilesetBlocos = this.tilemapMapa.addTileset('bloco')
+        //this.sound.add('mapa', {loop:true}).play()
+        //this.tilemapMapa = this.make.tilemap() [{
+        //    key: 'mapa'
+        //}]
 
-        this.layerTerreno = this.tilemapMapa.createLayer('terreno', [this.tilesetGrama])
-        }
+        //this.tilesetBlocos = this.tilemapMapa.addTileset('bloco')
+
+        //this.layerTerreno = this.tilemapMapa.createLayer('terreno', [this.tilesetGrama])
+                
+        this.personagem = this.physics.add.sprite(400, 255, 'cavaleiro-1')
+
+        this.anims.create({ 
+          key: 'cavaleiro-1-idle',
+          frames: this.anims.generateFrameNumbers('cavaleiro-1', {start: 0, end: 1}),
+          frameRate: 1,
+          repeat: -1
+        })
+
+        this.anims.create({
+          key: 'cavaleiro-1-walkingLeft',
+          frames: this.anims.generateFrameNumbers('cavaleiro-1', {start: 0, end: 1}),
+          frameRate: 1,
+          repeat: -1
+        })
+
+        this.anims.create({
+          key: 'cavaleiro-1-walkingRight',
+          frames: this.anims.generateFrameNumbers('cavaleiro-1', {start: 1, end: 3}),
+          frameRate: 8,
+          repeat: -1
+        })
+
+        this.personagem.setInteractive().on('pointerdown', () => {
+          this.personagem.anims.play('cavaleiro-1-walkingRight')
+          this.personagem.setVelocityX(25)
+        })
+
+      }
   
       update(){
       }
--- cliente/js/sala.js
+++ cliente/js/sala.js
@@ -6,15 +6,16 @@ export default class sala extends Phaser.Scene {
       preload() {
         this.load.image('fundo', './assets/fundo.png')
         this.load.audio('iniciar', './assets/iniciar.mp3')
-        this.load.audio('mapa', './assets/mapa.mp3')
+        
       }
       create() {
         this.iniciar = this.sound.add('iniciar')
-        this.sound.add('mapa', {loop:true}).play()
         this.add.image(400,255,'fundo')
         this.add.text(100, 50, 'sala 1').setInteractive()
         .on('pointerdown', () => {
             this.iniciar.play()
+            this.game.scene.stop('sala')
+            this.game.scene.start('mapa')
         })
         }
   
