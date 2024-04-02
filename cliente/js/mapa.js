7351251953fdfd2136922913ac7a0a0d49117976
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-04-02 12:13:43 +0000

    Animando com botao

--- cliente/js/mapa.js
+++ cliente/js/mapa.js
@@ -10,10 +10,10 @@ export default class mapa extends Phaser.Scene {
 
         this.load.spritesheet('cavaleiro-1', './assets/entities/jumpKing.png', {frameWidth: 32, frameHeight: 32})
 
-        this.load.spritesheet('cima', '/assets/cima.png', {frameWidth: 64, frameHeight: 64})
-        this.load.spritesheet('baixo', '/assets/baixo.png', {frameWidth: 64, frameHeight: 64})
-        this.load.spritesheet('direita', '/assets/direita.png', {frameWidth: 64, frameHeight: 64})
-        this.load.spritesheet('esquerda', '/assets/esquerda.png', {frameWidth: 64, frameHeight: 64})
+        this.load.spritesheet('cima', '/assets/ui/cima.png', {frameWidth: 64, frameHeight: 64})
+        this.load.spritesheet('baixo', '/assets/ui/baixo.png', {frameWidth: 64, frameHeight: 64})
+        this.load.spritesheet('direita', '/assets/ui/direita.png', {frameWidth: 64, frameHeight: 64})
+        this.load.spritesheet('esquerda', '/assets/ui/esquerda.png', {frameWidth: 64, frameHeight: 64})
       }
   
       create() {
@@ -28,14 +28,7 @@ export default class mapa extends Phaser.Scene {
                 
         this.personagem = this.physics.add.sprite(400, 255, 'cavaleiro-1')
 
-        this.baixo = this.add.sprite(100, 400, 'cima', 0)
-        .setScrollFactor(0)
-        .setInteractive()
-        .on('pointerover', ()=>{
-          this.cima.setFrame(1)
-        }).on('pointerout', () => {
-          this.cima.setFrame(0)
-        })
+        
 
         this.anims.create({ 
           key: 'cavaleiro-1-idle',
@@ -47,7 +40,7 @@ export default class mapa extends Phaser.Scene {
         this.anims.create({
           key: 'cavaleiro-1-walkingLeft',
           frames: this.anims.generateFrameNumbers('cavaleiro-1', {start: 0, end: 1}),
-          frameRate: 1,
+          frameRate: ,
           repeat: -1
         })
 
@@ -58,9 +51,28 @@ export default class mapa extends Phaser.Scene {
           repeat: -1
         })
 
-        this.personagem.setInteractive().on('pointerdown', () => {
-          this.personagem.anims.play('cavaleiro-1-walkingRight')
-          this.personagem.setVelocityX(25)
+
+        this.esquerda = this.add.sprite(100, 400, 'esquerda', 0)
+        .setScrollFactor(0)
+        .setInteractive()
+        .on('pointerover', ()=>{
+          this.personagem.anims.play('cavaleiro-1-walkingLeft')
+          this.esquerda.setFrame(1)
+          this.personagem.setVelocityX(-50)
+        }).on('pointerout', () => {
+          this.esquerda.setFrame(0)
+          this.personagem.setVelocityX(0)
+        })
+
+        this.direita = this.add.sprite(180, 400, 'direita', 0)
+        .setScrollFactor(0)
+        .setInteractive()
+        .on('pointerover', ()=>{
+          this.direita.setFrame(1)
+          this.personagem.setVelocityX(50)
+        }).on('pointerout', () => {
+          this.direita.setFrame(0)
+          this.personagem.setVelocityX(0)
         })
 
       }
