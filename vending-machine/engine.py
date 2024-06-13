6106031219b949fdbd48406bdfb5b90a9650b194
Ederson Torresini <etorresini@ifsc.edu.br>
2024-06-13 00:04:37 -0300

    Testar comando em background com thread

--- vending-machine/engine.py
+++ vending-machine/engine.py
@@ -1,27 +1,36 @@
 from flask import Flask, request, jsonify
 from time import sleep
+import threading
 
 app = Flask(__name__)
 busy = False
 
-@app.route('/engine', methods=['POST'])
+
+def background_command():
+    sleep(10)
+    print("Product processed successfully")
+    return
+
+
+@app.route("/engine", methods=["POST"])
 def engine():
     global busy
     if busy:
         return "Service Unavailable", 503
-    
+
     data = request.get_json()
     try:
-        if data['product'] == '1': 
+        if data["product"] == "1":
             busy = True
-            # Ativa o motor 1
-            sleep(10)
+            threading.Thread(target=background_command).start()
+            print("Product processing started")
             busy = False
             return "", 200
     except:
         pass
-    
+
     return "Bad Request", 400
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
