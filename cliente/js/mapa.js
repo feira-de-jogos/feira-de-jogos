1c02a5ac8f709a13c706112c40ed206c30a0fec8
Guilherme058802 <160231887+Guilherme058802@users.noreply.github.com>
2024-07-02 12:23:02 +0000

    Tentativa 1 de Joystick

--- cliente/js/mapa.js
+++ cliente/js/mapa.js
@@ -1,13 +1,11 @@
-export default class abertura extends Phaser.Scene {
+export default class tilemapMapa extends Phaser.Scene {
   constructor () {
     super('mapa')
   }
 
   preload () {
     this.load.audio('mapa', './assets/mapa.mp3')
-
     this.load.tilemapTiledJSON('mapa', './assets/mapa/mapa.json')
-
     this.load.image('blocos', './assets/mapa/blocos.png')
     this.load.image('grama', './assets/mapa/grama.png')
     this.load.image('itens', './assets/mapa/itens.png')
@@ -22,60 +20,14 @@ export default class abertura extends Phaser.Scene {
     this.load.spritesheet('baixo', './assets/baixo.png', { frameWidth: 64, frameHeight: 64 })
     this.load.spritesheet('esquerda', './assets/esquerda.png', { frameWidth: 64, frameHeight: 64 })
     this.load.spritesheet('direita', './assets/direita.png', { frameWidth: 64, frameHeight: 64 })
+
+    // Carrega o plugin do joystick virtual
+    this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true)
   }
 
   create () {
-    // this.sound.add('mapa', { loop: true }).play()
-
-    this.cima = this.add.sprite(100, 250, 'cima', 0)
-      .setScrollFactor(0)
-      .setInteractive()
-      .on('pointerover', () => {
-        this.cima.setFrame(1)
-        this.personagem.setVelocityY(-50)
-      })
-      .on('pointerout', () => {
-        this.cima.setFrame(0)
-        this.personagem.setVelocityY(0)
-      })
-
-    this.baixo = this.add.sprite(100, 350, 'baixo', 0)
-      .setScrollFactor(0)
-      .setInteractive()
-      .on('pointerover', () => {
-        this.baixo.setFrame(1)
-        this.personagem.setVelocityY(50)
-      })
-      .on('pointerout', () => {
-        this.baixo.setFrame(0)
-        this.personagem.setVelocityY(0)
-      })
-
-    this.esquerda = this.add.sprite(600, 350, 'esquerda', 0)
-      .setScrollFactor(0)
-      .setInteractive()
-      .on('pointerover', () => {
-        this.esquerda.setFrame(1)
-        this.personagem.setVelocityX(-50)
-      })
-      .on('pointerout', () => {
-        this.esquerda.setFrame(0)
-        this.personagem.setVelocityX(0)
-      })
-
-    this.direita = this.add.sprite(700, 350, 'direita', 0)
-      .setScrollFactor(0)
-      .setInteractive()
-      .on('pointerover', () => {
-        this.direita.setFrame(1)
-        this.personagem.setVelocityX(50)
-      })
-      .on('pointerout', () => {
-        this.direita.setFrame(0)
-        this.personagem.setVelocityX(0)
-      })
+    // Configuração do mapa e do personagem
     this.tilemapMapa = this.make.tilemap({ key: 'mapa' })
-
     this.tilesetBlocos = this.tilemapMapa.addTilesetImage('blocos')
     this.tilesetGrama = this.tilemapMapa.addTilesetImage('grama')
     this.tilesetItens = this.tilemapMapa.addTilesetImage('itens')
@@ -91,25 +43,21 @@ export default class abertura extends Phaser.Scene {
     this.layerPlantas = this.tilemapMapa.createLayer('plantas', [this.tilesetPlantas])
     this.layerItens = this.tilemapMapa.createLayer('itens', [this.tilesetItens])
     this.layerParedes = this.tilemapMapa.createLayer('paredes', [this.tilesetBlocos, this.tilesetParedes])
-    // É necessário deixar claro no código qual tileset foi utilizado em cada camada e referenciá-lo
 
+    // Criação do personagem e suas animações
     this.personagem = this.physics.add.sprite(400, 225, 'salsicha-caramelo')
-
-    // Essa parte abaixo do código cria a animação do personagem
     this.anims.create({
       key: 'salsicha-caramelo-parado',
       frames: this.anims.generateFrameNumbers('salsicha-caramelo', { start: 0, end: 1 }),
       frameRate: 2,
       repeat: -1
     })
-
     this.anims.create({
       key: 'salsicha-caramelo-direita',
       frames: this.anims.generateFrameNumbers('salsicha-caramelo', { start: 3, end: 1 }),
       frameRate: 7,
       repeat: -1
     })
-
     this.anims.create({
       key: 'salsicha-caramelo-esquerda',
       frames: this.anims.generateFrameNumbers('salsicha-caramelo', { start: 8, end: 6 }),
@@ -118,16 +66,54 @@ export default class abertura extends Phaser.Scene {
     })
     this.personagem.anims.play('salsicha-caramelo-parado')
 
-    // Essa parte abaixo chama a animação a partir de uma ação definida
-    this.personagem
-      .setInteractive()
-      .on('pointerdown', () => {
-        this.personagem.anims.play('salsicha-caramelo-esquerda')
-        this.personagem.setVelocityY(0)
-        this.personagem.setVelocityX(-50)
-      })
+    // Configuração do plugin do joystick virtual
+    this.joystick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+      x: 100,
+      y: 500,
+      radius: 50,
+      base: this.add.circle(100, 500, 50, 0x888888),
+      thumb: this.add.circle(100, 500, 25, 0xcccccc)
+    })
+
+    // Lógica para movimentar o personagem com base no joystick virtual
+    this.joystick.on('update', this.handleJoystickMove, this)
+
+    // Início do follow da câmera
     this.cameras.main.startFollow(this.personagem)
   }
-}
 
-// update () {
+  update () {
+    this.handleJoystickMove()
+  }
+
+  handleJoystickMove () {
+    const speed = 100 // Velocidade do movimento do personagem
+
+    // Movimenta o personagem com base na direção do joystick
+    const angle = this.joystick.angle
+    const distance = this.joystick.force
+
+    this.personagem.setVelocityX(Math.cos(angle) * speed * distance)
+    this.personagem.setVelocityY(Math.sin(angle) * speed * distance)
+
+    // Animação do personagem conforme a direção do movimento
+    if (distance > 0.1) {
+      if (angle > -Math.PI / 4 && angle < Math.PI / 4) {
+        // Direita
+        this.personagem.anims.play('salsicha-caramelo-direita', true)
+      } else if (angle > Math.PI / 4 && angle < 3 * Math.PI / 4) {
+        // Para baixo
+        this.personagem.anims.play('salsicha-caramelo-parado', true)
+      } else if (angle < -Math.PI / 4 && angle > -3 * Math.PI / 4) {
+        // Para cima
+        this.personagem.anims.play('salsicha-caramelo-esquerda', true)
+      } else {
+        // Esquerda
+        this.personagem.anims.play('salsicha-caramelo-esquerda', true)
+      }
+    } else {
+      // Se a força do joystick for baixa, o personagem para
+      this.personagem.anims.play('salsicha-caramelo-parado', true)
+    }
+  }
+}
