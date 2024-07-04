072f4f5f5f168b9013363513feacd091d1923fc3
nickzinon <160231606+nickzinon@users.noreply.github.com>
2024-07-04 11:26:49 +0000

    ajustes

--- cliente/js/abertura.js
+++ cliente/js/abertura.js
@@ -11,6 +11,13 @@ export default class abertura extends Phaser.Scene {
       this.add.image(400,255,'fundo')
         .setInteractive()
         .on('pointerdown', () => {
+
+          navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+          .then((stream) => {
+            globalThis.game.midias = stream
+          })
+          .catch((error) => console.error(error))
+
           this.game.scene.stop('abertura')
           this.game.scene.start('sala')
         })
