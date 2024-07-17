8cdb0d055a07176239afd1687921f95c25e6beee
Léo Jung Neves <leo.jn13@aluno.ifsc.edu.br>
2024-07-16 23:47:03 -0300

    Testar aplicação simples #20

--- vending-machine/websocket/wss-client.py
+++ vending-machine/websocket/wss-client.py
@@ -2,29 +2,47 @@ from os import getenv
 from dotenv import load_dotenv
 import asyncio
 import socketio
+import jwt
 
 load_dotenv()
-secret_key = getenv('SECRET_KEY')
+url = getenv("URL", default="wss://feira-de-jogos.dev.br")
+socketio_path = getenv("SOCKETIO_PATH", default="/api/v2/machine")
+namespace = getenv("NAMESPACE", default="/vending-machine")
+jwt_algorithm = getenv("JWT_ALGORITHM", default="HS256")
+secret_key = getenv("SECRET_KEY", default="")
 
 sio = socketio.AsyncClient()
 
-@sio.event
-async def connect():
-    print('connection established')
 
 @sio.event
-async def my_message(data):
-    print('message received with ', data)
-    await sio.emit('my response', {'response': 'my response'})
+async def connect():
+    print("connection established")
+
+
+@sio.event
+async def onState(data):
+    print("message received with ", data)
+    await sio.emit("my response", {"response": "my response"})
+
 
 @sio.event
 async def disconnect():
-    print('disconnected from server')
+    print("disconnected from server")
+
 
 async def main():
-    await sio.connect('wss://feira-de-jogos.dev.br/sssvending-machine', socketio_path='/api/v2/machine')
-    await sio.emit('state', 'ready')
+    token = jwt.encode(
+        {"machine": "vending-machine", "id": 0}, secret_key, algorithm=jwt_algorithm
+    )
+    await sio.connect(
+        url,
+        socketio_path=socketio_path,
+        namespaces=namespace,
+        auth={"token": token},
+    )
+    await sio.emit("state", "ready", namespace=namespace)
     await sio.wait()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
