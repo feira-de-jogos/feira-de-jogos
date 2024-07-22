c76f0ef10549992e0b8027114ce693b9255dd618
Ederson Torresini <etorresini@ifsc.edu.br>
2024-07-21 22:55:53 -0300

    Preparar depurador Python

--- vending-machine/websocket/wss-client.py
+++ vending-machine/websocket/wss-client.py
@@ -4,12 +4,14 @@ import asyncio
 import socketio
 import jwt
 
+
 load_dotenv()
 url = getenv("URL", default="wss://feira-de-jogos.dev.br")
 socketio_path = getenv("SOCKETIO_PATH", default="/api/v2/machine")
 namespace = getenv("NAMESPACE", default="/vending-machine")
 jwt_algorithm = getenv("JWT_ALGORITHM", default="HS256")
 secret_key = getenv("TOKEN_SECRET_KEY_VENDING_MACHINE", default="")
+
 
 sio = socketio.AsyncClient()
 
