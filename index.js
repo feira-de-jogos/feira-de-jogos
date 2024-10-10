d243c9acc7b9efff51f84de2f2d46a3636596f5b
inacio18 <kevin.a2004@aluno.ifsc.edu.br>
2024-10-10 19:00:18 +0000

    arrumado

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import sala from './sala.js'
 class Game extends Phaser.Game {
   constructor () {
     super(config)
+
     this.scene.add('abertura', abertura)
     this.scene.add('sala', sala)
     this.scene.start('abertura')
   }
 }
+
 window.onload = () => {
   window.game = new Game()
 }
