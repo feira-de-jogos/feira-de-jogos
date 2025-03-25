fff4e8bed2e1a932902e3b730082d34544ad3860
Ederson Torresini <etorresini@ifsc.edu.br>
2025-03-25 17:04:46 -0300

    Unificar servidores Node

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -13,7 +13,7 @@ class Game extends Phaser.Game {
 
     let iceServers
     if (window.location.host === 'feira-de-jogos.dev.br') {
-      this.socket = io({ path: '/adcieqipt20241-vimdoalegrete-jogo/socket.io/' })
+      this.socket = io({ path: '/api/v2/game/' })
       iceServers = [
         {
           urls: 'turn:feira-de-jogos.dev.br',
--- cliente/js/sala.js
+++ cliente/js/sala.js
@@ -72,7 +72,7 @@ export default class sala extends Phaser.Scene {
           this.iniciar.play()
 
           // Define a variável global da sala
-          globalThis.game.sala = sala.numero
+          globalThis.game.sala = 'adcieqipt20241-vimdoalegrete-jogo/' + sala.numero
 
           // Emite o evento 'entrar-na-sala' para o servidor
           globalThis.game.socket.emit('entrar-na-sala', globalThis.game.sala)
