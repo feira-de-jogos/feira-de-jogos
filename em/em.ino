08ba564d7c53dcb130cc52d239a4a2b7dc04a93f
Ederson Torresini <etorresini@ifsc.edu.br>
2025-04-22 19:11:16 +0000

    Adaptar código para mensagem como Influx data point

--- em/em.ino
+++ em/em.ino
@@ -1,6 +1,4 @@
 // Bibliotecas
-#include <Arduino.h>
-#include <string.h>
 #include <PubSubClient.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
@@ -9,141 +7,98 @@
 #include <Adafruit_BMP280.h>
 #include <WiFi.h>
 
+// Configuração
+#include "em.h"
 
-//VARIAVEIS PARA MQTT
-#define SSID "feira"
-#define PASSWORD "feiradejogos"
-#define MQTT_SERVER "feira-de-jogos.dev.br"
-#define MQTT_PORT 1883
-#define MQTT_CLIENT_ID "EMv0"
-String MENSAGEM = "";
-String TOPICO = "/em/v0";
-
-#define MQ2G "0000.00"
-#define MQ5G "0000.00"
-#define MQ7G "0000.00"
-#define D18T "0000.00"
-#define BMPT "0000.00"
-#define BMPP "0000.00"
-#define BMPA "0000.00"
-#define DHTU "0000.00"
-#define DHTT "0000.00"
-#define L35T "0000.00"
-
-
-
-// DEFINIÇÕES DE REDE/MQTT
+// Objetos
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-
-// Pré-definições
-int Sensor_MQ2 = 36;        // 
-int Sensor_MQ5 = 39;        // 
-int Sensor_MQ7 = 34;        // Monóxido de Carbono (CO)
-int Sensor_DHT11 = 35;      // Umidade e Temperatura
-int Sensor_DS18B20 = 32;    // Temperatura
-int Sensor_LM35 = 33;         // Temperatura
-
-DHT dht(Sensor_DHT11, DHT11);
-OneWire oneWire(Sensor_DS18B20);
+DHT dht(sensor_DHT11, DHT11);
+OneWire oneWire(sensor_DS18B20);
 DallasTemperature sensors(&oneWire);
 DeviceAddress endereco;
 Adafruit_BMP280 bmp;
 
-// Periféricos
-int LED = 2;                        // LED Built-in
-float leitura[10] = {0,0,0,0,0,0,0,0,0,0};
+// Variáveis globais
+String mensagem = "";
+float leitura;
 
-
-// Variáveis Globais
-unsigned long tempoAnterior = 0;
-const long intervalo = 400;
-
-void setup() {
-  pinMode(LED,OUTPUT);
-  pinMode(Sensor_MQ2,INPUT);
-  pinMode(Sensor_MQ5,INPUT);
-  pinMode(Sensor_MQ7,INPUT);
-  pinMode(Sensor_DHT11,INPUT);
-  pinMode(Sensor_DS18B20,INPUT);
-  pinMode(Sensor_LM35,INPUT);
-  WiFi.begin(SSID, PASSWORD);
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  sensors.begin();
-  digitalWrite(LED, LOW);
-  dht.begin();
-  Serial.begin(115200);
+void ler_sensores(){
+  dtostrf(analogRead(sensor_MQ2), 6, 2, MQ2G);
+  dtostrf(analogRead(sensor_MQ5), 6, 2, MQ5G);
+  dtostrf(analogRead(sensor_MQ7), 6, 2, MQ7G);
+  dtostrf(sensors.requestTemperatures(), 6, 2, D18T);
+  dtostrf(bmp.readTemperature(), 6, 2, BMPT);
+  dtostrf(bmp.readPressure(), 6, 2, BMPP);
+  dtostrf(bmp.readAltitude(1013.25), 6, 2, BMPA);
+  dtostrf(dht.readHumidity(), 6, 2, DHTU);
+  dtostrf(dht.readTemperature(1), 6, 2, DHTT);
+  dtostrf(((analogRead(sensor_LM35) / 1024.0) * 3300) / 10, 6, 2, L35T);
 }
 
-void loop() {
-  unsigned long tempoAtual = millis();
-  if (tempoAtual - tempoAnterior >= intervalo) {
-    tempoAnterior = tempoAtual;
-    leitura[1] = analogRead(Sensor_MQ5);
-    leitura[2] = analogRead(Sensor_MQ7);
-    leitura[3] = sensors.requestTemperatures();
-    leitura[4] = bmp.readTemperature();
-    leitura[5] = bmp.readPressure();
-    leitura[6] = bmp.readAltitude(1013.25);
-    leitura[7] = dht.readHumidity();
-    leitura[8] = dht.readTemperature(1);
-    leitura[9] = ((analogRead(Sensor_LM35)/1024.0) * 3300)/10;   // Leitura LM35 em ºC
+String formatar_mensagem()
+{
+  String msg = "";
 
-    Serial.println("----------------");
-    Serial.print("Tempo atual = ");
-    Serial.println(tempoAtual);
+  msg += mqtt_client_id;
+  msg += " ";
+  msg += "MQ2G=" + String(MQ2G) + ",";
+  msg += "MQ5G=" + String(MQ5G) + ",";
+  msg += "MQ7G=" + String(MQ7G) + ",";
+  msg += "D18T=" + String(D18T) + ",";
+  msg += "BMPT=" + String(BMPT) + ",";
+  msg += "BMPP=" + String(BMPP) + ",";
+  msg += "BMPA=" + String(BMPA) + ",";
+  msg += "DHTU=" + String(DHTU) + ",";
+  msg += "DHTT=" + String(DHTT) + ",";
+  msg += "L35T=" + String(L35T);
+  // msg += " " + timestamp
+  return msg;
+}
 
-    dtostrf(leitura[0], 6, 2, MQ2G);
-    dtostrf(leitura[1], 6, 2, MQ5G);
-    dtostrf(leitura[2], 6, 2, MQ7G);
-    dtostrf(leitura[3], 6, 2, D18T);
-    dtostrf(leitura[4], 6, 2, BMPT);
-    dtostrf(leitura[5], 6, 2, BMPP);
-    dtostrf(leitura[6], 6, 2, BMPA);
-    dtostrf(leitura[7], 6, 2, DHTU);
-    dtostrf(leitura[8], 6, 2, DHTT);
-    dtostrf(leitura[9], 6, 2, L35T);
-    Serial.println("----------------");
-    /*Serial.print("Leitura MQ2: ");
-    Serial.println(leitura[0]);
-    Serial.print("Captura MQ5: ");
-    Serial.println(leitura[1]);
-    Serial.print("Captura MQ7: ");
-    Serial.println(leitura[2]);
-    Serial.print("Temperatura DS18B20: ");
-    Serial.println(leitura[3]);
-    Serial.print("Temperatura BMP280: ");
-    Serial.println(leitura[4]);
-    Serial.print("Pressão BMP280: ");
-    Serial.println(leitura[5]);
-    Serial.print("Altitude BMP280: ");
-    Serial.println(leitura[6]);
-    Serial.print("Umidade DHT11: ");
-    Serial.println(leitura[7]);
-    Serial.print("Temperatura DHT11: ");
-    Serial.println(leitura[8]);
-    Serial.print("Temperatura LM35: ");
-    Serial.println(L35T);
-    Serial.println("----------------");*/
-  }  
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(sensor_MQ2, INPUT);
+  pinMode(sensor_MQ5, INPUT);
+  pinMode(sensor_MQ7, INPUT);
+  pinMode(sensor_DHT11, INPUT);
+  pinMode(sensor_DS18B20, INPUT);
+  pinMode(sensor_LM35, INPUT);
 
-  if (!client.connected())
+  dht.begin();
+  sensors.begin();
+  WiFi.begin(ssid, password);
+  client.setServer(mqtt_server, mqtt_port);
+  Serial.begin(115200);
+
+  digitalWrite(LED, LOW);
+}
+
+void loop()
+{
+  ler_sensores();
+
+  if (client.connected())
+  {
+    mensagem = formatar_mensagem();
+    client.publish(topico, mensagem.c_str());
+
+    client.loop();
+  }
+  else
   {
     digitalWrite(LED, LOW);
-    if (client.connect(MQTT_CLIENT_ID))
+    if (client.connect(mqtt_client_id))
     {
       Serial.println("Conectado ao broker MQTT!");
       digitalWrite(LED, HIGH);
-    } else {
-      Serial.print("Broker MQTT: reconectando em 5s...");
-      delay(1000);
     }
-  } else {
-    client.loop();
+    else
+    {
+      Serial.println("Broker MQTT: reconectando em 1s...");
+    }
   }
-  
-  MENSAGEM = MQ2G + MQ5G + MQ7G + D18T + BMPT + BMPP + BMPA + DHTU + DHTT + L35T;
-  client.publish(TOPICO, MENSAGEM);
 
+  delay(1000);
 }
