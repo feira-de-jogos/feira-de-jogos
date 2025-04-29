56a342ce938e8ed4b334e1d752466e50f0efad59
Ederson Torresini <etorresini@ifsc.edu.br>
2025-04-29 17:17:43 -0300

    Corrigir chave do Wi-Fi

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from machine import Pin
 import network
 from time import sleep
+import mip
 
 
 ssid = "em"
-key = "estacao-meteorlogica"
+key = "estacao-meteorologica"
 
 p2 = Pin(2, Pin.OUT)
 
@@ -15,3 +16,4 @@ wlan.connect(ssid, key)
 sleep(3)
 if wlan.isconnected():
     p2.on()
+    mip.install("umqtt.simple")    
