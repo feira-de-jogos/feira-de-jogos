05649e3bd48785f17c207316144073f9a3408bb5
viniciusam11 <vinicius.am11@aluno.ifsc.edu.br>
2025-09-11 08:48:55 -0300

    Código usado no teste de umidade na EMv1 #65

--- equipamentos/estacao-meteorologica/micropython/em_v1/main.py
+++ equipamentos/estacao-meteorologica/micropython/em_v1/main.py
@@ -11,8 +11,8 @@ led.off()
 
 uuid = '88a2e875-30a9-49b5-ab15-4a185eaa85e3'
 topico_data = 'em/' + uuid 
-SSID = ''
-PASSWORD = ''
+SSID = 'INTERNET'
+PASSWORD = '33467840'
 versao_em = 1
 
 latitude = '-27.55256'
@@ -28,23 +28,12 @@ i2c1 = I2C(1, scl=Pin(26), sda=Pin(25))
 print("Dispositivos I2C0 encontrados:", i2c0.scan())
 print("I2C1 encontrados:", i2c1.scan())
 
-ds18x20 = ds18x20.DS18X20(onewire.OneWire(Pin(15)))
-sleep_ms(200)
-roms = ds18x20.scan()
-print(roms)
 aht10 = ahtx0.AHT10(i2c0, 56)
 aht25 = aht25.AHT25(i2c1)
-mcp9808 = mcp9808.MCP9808(i2c0)
-mcp9808.set_resolution(3)
-bmp180 = bmp180.BMP180(i2c=i2c0)
-bmp180.oversample_sett = 3
-bmp280 = bmp280.BMP280(i2c=i2c1, i2c_address=118)
 bme280 = bme280.BME280(i2c=i2c0, addr=118)
-bmp388 = bmp388.DFRobot_BMP388_I2C(i2c1)
 ds3231 = ds3231.DS3231(i2c0)
 dht22 = dht.DHT22(Pin(27))
 dht11 = dht.DHT11(Pin(0))
-lm75 = lm75a.LM75A(i2c=i2c0)
 adc = ads1x15.ADS1115(i2c1, address=72, gain=0)
 
 def timestamp():
@@ -69,12 +58,6 @@ def ds18b20_ler():
         print(msg_erro)
         return 0.0
 
-def ler_lm35dz():
-    valor = adc.read(0, 0)
-    tensao = (6.144 * valor) / 32768
-    temp = tensao / 0.01
-    return temp
-
 def conecta_wifi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -85,80 +68,56 @@ def conecta_wifi():
             sleep(0.5)
             print('CONECTANDO')
         print("Wi-Fi conectado:", wlan.ifconfig())
+        
 try:
-    AHT25_RH, dados['temp.aht25'], AHT25_status, AHT25_CRC = aht25.read_sensor()
+    dados['umid.aht25'], AH25_TEMP, AHT25_status, AHT25_CRC = aht25.read_sensor()
 except Exception as e:
     print('erro aht25', e)
 sleep(0.05)
     
+
 try:
-    dados["temp.bme280"] = bme280.temperature()
+    temp_tmp = bme280.temperature()
+    dados["umid.bme280"] = bme280.humidity()
 except Exception as e:
     print('erro bme280', e)
-    dados["temp.bme280"] = 0.0
 sleep(0.05)
-        
+    
 conecta_wifi()
 client = MQTTClient(topico_data,'mqtt.feira-de-jogos.dev.br', port=1883)
 client.connect()
 
 while True:
     
-    dados["temp.bmp280"] = bmp280.get_temperature()
-    
     try:
-        dados["temp.bme280"] = bme280.temperature()
+        temp_tmp = bme280.temperature()
+        dados["umid.bme280"] = bme280.humidity()
     except Exception as e:
         print('erro bme280', e)
     sleep(0.05)
     
-    try:
-        dados['temp.bmp388'] = bmp388.readTemperature()
-    except Exception as e:
-        print('erro bmp388', e)
-    sleep(0.05)
     
     try:
-        dados['temp.aht10'] = aht10.temperature
+        dados['umid.aht10'] = aht10.relative_humidity
     except Exception as e:
         print('erro aht10', e)
     sleep(0.05)
-    
+
+
     try:
-        dados['temp.mcp9808'] = mcp9808.get_temp()
-    except Exception as e:
-        print('erro mcp9808', e)
-    sleep(0.05)
-    
-    try:
-        dados['temp.bmp180'] = bmp180.temperature
-    except Exception as e:
-        print('erro bmp180', e)
-    sleep(0.05)
-    
-    try:
-        dados['temp.lm75'] = lm75.temp()
-    except Exception as e:
-        print('erro lm75', e)
-    sleep(0.05)
-    
-    try:
-        AHT25_RH, dados['temp.aht25'], AHT25_status, AHT25_CRC = aht25.read_sensor()
+        dados['umid.aht25'], AHT25_TEMP, AHT25_status, AHT25_CRC = aht25.read_sensor()
     except Exception as e:
         print('erro aht25', e)
     sleep(0.05)
 
-    dados['temp.lm35dz'] = ler_lm35dz()
-    
-    dados['temp.ds18b20'] = ds18b20_ler() 
     
     dht22.measure()
     sleep(1)
-    dados['temp.dht22'] = dht22.temperature()
+    dados['umid.dht22'] = dht22.humidity()
     
     dht11.measure()
     sleep(3)
-    dados['temp.dht11'] = dht11.temperature()
+    dados['umid.dht11'] = dht11.humidity()
     
     ts = timestamp()
     print(ts)
@@ -170,30 +129,16 @@ while True:
     data += 'lng=' + str(longitude) + ','
     data += 'alt=' + str(altitude)
     data += ' '
+        
+    data += 'umid.bme280=' + str(dados['umid.bme280']) + ','
+            
+    data += 'umid.aht10=' + str(dados['umid.aht10']) + ','
     
-    data += 'temp.bmp280=' + str(dados['temp.bmp280']) + ','
+    data += 'umid.aht25=' + str(dados['umid.aht25']) + ','
+                
+    data += 'umid.dht22=' + str(dados['umid.dht22']) + ','
     
-    data += 'temp.bme280=' + str(dados['temp.bme280']) + ','
-    
-    data += 'temp.bmp388=' + str(dados['temp.bmp388']) + ','
-    
-    data += 'temp.bmp180=' + str(dados['temp.bmp180']) + ','
-    
-    data += 'temp.aht10=' + str(dados['temp.aht10']) + ','
-    
-    data += 'temp.aht25=' + str(dados['temp.aht25']) + ','
-    
-    data += 'temp.lm35dz=' + str(dados['temp.lm35dz']) + ','
-    
-    data += 'temp.lm75=' + str(dados['temp.lm75']) + ','
-
-    data += 'temp.mcp9808=' + str(dados['temp.mcp9808']) + ','
-    
-    data += 'temp.ds18b20=' + str(dados['temp.ds18b20']) + ','
-    
-    data += 'temp.dht22=' + str(dados['temp.dht22']) + ','
-    
-    data += 'temp.dht11=' + str(dados['temp.dht11'])
+    data += 'umid.dht11=' + str(dados['umid.dht11'])
     
     data += ' ' + str(ts)
     
@@ -204,7 +149,3 @@ while True:
     
     print(data)
     sleep(30)
-
-
-
-
